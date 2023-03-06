735f4e00bc8e9cfedc91d3ee647373e58f56593f
Alexander Herranz <alexherranz89@gmail.com>
2023-03-07 00:41:54 +0100

    Update Crowdsale-v8.sol

--- Crowdsale-v8.sol
+++ Crowdsale-v8.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.1;
+pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/utils/Context.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/token/ERC20/IERC20.sol";
